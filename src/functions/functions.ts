dab4c51498f3937146f672980856bcec6e78e41f
Unerty <johnny.unerty@gmail.com>
2020-11-26 09:34:34 +0200

    Rename functions

--- src/functions/functions.ts
+++ src/functions/functions.ts
@@ -2,48 +2,48 @@ export function Mason() {
   return 0;
 }
 
-export function binomialDistribution(s: number, n: number, p: number) {
+export function binomialDistributionMomentGeneratingFunction(s: number, n: number, p: number) {
   return Math.pow((p * Math.pow(Math.E, s) + 1 - p), n);
 }
 
-export function discreteDistribution(s: number, pArray: Array<number>, TArray: Array<number>) {
+export function discreteDistributionMomentGeneratingFunction(s: number, pArray: Array<number>, TArray: Array<number>) {
   let nominator: number = 0;
   let denominatior: number = 0;
-    pArray.forEach((p, index) => {
+  pArray.forEach((p, index) => {
     nominator += (p * Math.pow(Math.E, (s * TArray[index])));
     denominatior += p;
   });
-  return (nominator/denominatior);
+  return (nominator / denominatior);
 }
 
-export function exponentialDistribution(s: number, a: number) {
+export function exponentialDistributionMomentGeneratingFunction(s: number, a: number) {
   return Math.pow((1 - (s / a)), -1);
 }
 
-export function gammaDistribution(s: number, a: number, b: number) {
+export function gammaDistributionMomentGeneratingFunction(s: number, a: number, b: number) {
   return Math.pow((1 - (s / a)), (-1 * b));
 }
 
-export function geometricalDistribution(s: number, p: number) {
+export function geometricalDistributionMomentGeneratingFunction(s: number, p: number) {
   let nominator: number = p * Math.pow(Math.E, s);
   let denominatior: number = (1 - Math.pow(Math.E, s) + p * Math.pow(Math.E, s));
-  return (nominator/denominatior);
+  return (nominator / denominatior);
 }
 
-export function negativeBinomialDistribution(s: number, p: number, r: number) {
-  return Math.pow(p/(1 - Math.pow(Math.E, s) + p * Math.pow(Math.E, s)), r);
+export function negativeBinomialDistributionMomentGeneratingFunction(s: number, p: number, r: number) {
+  return Math.pow(p / (1 - Math.pow(Math.E, s) + p * Math.pow(Math.E, s)), r);
 }
 
-export function normalDistribution(s: number, m: number, sigma: number) {
-  return Math.pow(Math.E, s * m + (1/2) * s*s * sigma*sigma);
+export function normalDistributionMomentGeneratingFunction(s: number, m: number, sigma: number) {
+  return Math.pow(Math.E, s * m + (1 / 2) * s * s * sigma * sigma);
 }
 
-export function puassonDistribution(s: number, lambda: number) {
-  return Math.pow(Math.E, lambda*(Math.pow(Math.E, s) - 1))
+export function puassonDistributionMomentGeneratingFunction(s: number, lambda: number) {
+  return Math.pow(Math.E, lambda * (Math.pow(Math.E, s) - 1));
 }
 
-export function continuousUniformDistribution(s: number, a: number, b: number) {
+export function continuousUniformDistributionMomentGeneratingFunction(s: number, a: number, b: number) {
   let nominator: number = Math.pow(Math.E, s * a) - Math.pow(Math.E, s * b);
   let denominatior: number = (a - b) * s;
-  return (nominator/denominatior);
+  return (nominator / denominatior);
 }

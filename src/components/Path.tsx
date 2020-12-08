635b0fdd8d968e55d5cf76954ef3d2963117d2c0
Unerty <johnny.unerty@gmail.com>
2020-12-08 17:16:53 +0200

    Display all inputs inside the pathes

--- src/components/Path.tsx
+++ src/components/Path.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import PathModel from '@/models/PathModel';
+import { Distribution } from '@/functions/constants';
 
 interface IProps extends PathModel {
   setAdditiveParameter: (additiveParameter: number) => void;
@@ -13,25 +14,99 @@ export default class Path extends React.Component<IProps> {
   }
 
   render() {
-    const { id, probability, additiveParameter, distribution, setAdditiveParameter, setProbability, setDistribution } = this.props;
+    const { BINOMIAL_DISTRIBUTION, CONTINIOUS_UNIFORM_DISTRIBUTION, EXPONENTIAL_DISTRIBUTION, GAMMA_DISTRIBUTION, GEOMETRICAL_DISTRIBUTION, NEGATIVE_BINOMIAL_DISTRIBUTION, NORMAL_DISTRIBUTION, PUASSON_DISTRIBUTION } = Distribution;
+
+    const nVisible = (
+      this.props.distribution === BINOMIAL_DISTRIBUTION ||
+      this.props.distribution === NEGATIVE_BINOMIAL_DISTRIBUTION
+    ) ? 'unset' : 'none';
+    const mVisible = this.props.distribution === NORMAL_DISTRIBUTION ? 'unset' : 'none';
+    const rVisible = this.props.distribution === NEGATIVE_BINOMIAL_DISTRIBUTION ? 'unset' : 'none';
+    const aVisible = (
+      this.props.distribution === EXPONENTIAL_DISTRIBUTION ||
+      this.props.distribution === GAMMA_DISTRIBUTION ||
+      this.props.distribution === CONTINIOUS_UNIFORM_DISTRIBUTION
+    ) ? 'unset' : 'none';
+    const bVisible = (
+      this.props.distribution === GAMMA_DISTRIBUTION ||
+      this.props.distribution === CONTINIOUS_UNIFORM_DISTRIBUTION
+    ) ? 'unset' : 'none';
+    const pVisible = (
+      this.props.distribution === BINOMIAL_DISTRIBUTION ||
+      this.props.distribution === GEOMETRICAL_DISTRIBUTION ||
+      this.props.distribution === NEGATIVE_BINOMIAL_DISTRIBUTION
+    ) ? 'unset' : 'none';
+    const lambdaVisible = this.props.distribution === PUASSON_DISTRIBUTION ? 'unset' : 'none';
+    const sigmaVisible = this.props.distribution === NORMAL_DISTRIBUTION ? 'unset' : 'none';
+    const { id, probability, additiveParameter, distribution, lambda, sigma, a, b, m, n, r, s, setAdditiveParameter, setProbability, setDistribution } = this.props;
     return (
       <div style={{
         backgroundColor: 'sandybrown',
-        display: 'inline-block',
-        width: '150px',
+        display: 'inline-grid',
+        width: '250px',
+        height: '270px',
         margin: '0.5rem',
         padding: '0.25rem',
       }}>
-        <h3>Номер дуги: {id}</h3>
-        <span>Вартість, грн.:<br/></span>
-        <input type="number" onChange={event => setAdditiveParameter(Number(event.target.value))}
-               value={additiveParameter.toString()} min={0} step={0.01}
-               style={{ width: '100px' }}
-        />
-        <br/>
-        <span>Ймовірність: <strong> {probability}</strong><br/></span>
-        <input type={'range'} onChange={event => setProbability(Number(event.target.value))}
-               value={probability.toString()} min={0} max={1} step={0.01}/>
+        <div>
+          <h3>Номер дуги: {id}</h3>
+          <span>Вартість, грн.:<br/></span>
+          <input type="number" onChange={event => setAdditiveParameter(Number(event.target.value))}
+                 value={additiveParameter.toString()} min={0} step={0.01}
+                 style={{ width: '100px' }}
+          />
+          <br/>
+          <span>Ймовірність: <strong> {probability}</strong><br/></span>
+          <input type={'range'} onChange={event => setProbability(Number(event.target.value))}
+                 value={probability.toString()} min={0} max={1} step={0.01}/><br/>
+          Розподіл:<br/>
+          <select value={this.props.distribution}
+                  onChange={event => setDistribution(event.target.value)}>
+            <option value={BINOMIAL_DISTRIBUTION}>{BINOMIAL_DISTRIBUTION}</option>
+            <option value={EXPONENTIAL_DISTRIBUTION}>{EXPONENTIAL_DISTRIBUTION}</option>
+            <option value={GAMMA_DISTRIBUTION}>{GAMMA_DISTRIBUTION}</option>
+            <option value={GEOMETRICAL_DISTRIBUTION}>{GEOMETRICAL_DISTRIBUTION}</option>
+            <option value={NEGATIVE_BINOMIAL_DISTRIBUTION}>{NEGATIVE_BINOMIAL_DISTRIBUTION}</option>
+            <option value={NORMAL_DISTRIBUTION}>{NORMAL_DISTRIBUTION}</option>
+            <option value={PUASSON_DISTRIBUTION}>{PUASSON_DISTRIBUTION}</option>
+            <option value={CONTINIOUS_UNIFORM_DISTRIBUTION}>{CONTINIOUS_UNIFORM_DISTRIBUTION}</option>
+          </select>
+        </div>
+        <div style={{ display: nVisible }}>
+          <br/>
+          n = <input value={this.props.n}
+                     onChange={event => this.setState({ n: Number(event.target.value) })}/><br/>
+        </div>
+        <div style={{ display: mVisible }}>
+          <br/>
+          m = <input value={this.props.m}
+                     onChange={event => this.setState({ m: Number(event.target.value) })}/><br/>
+        </div>
+        <div style={{ display: rVisible }}>
+          <br/>
+          r = <input value={this.props.r}
+                     onChange={event => this.setState({ r: Number(event.target.value) })}/><br/>
+        </div>
+        <div style={{ display: aVisible }}>
+          <br/>
+          a = <input value={this.props.a}
+                     onChange={event => this.setState({ a: Number(event.target.value) })}/><br/>
+        </div>
+        <div style={{ display: bVisible }}>
+          <br/>
+          b = <input value={this.props.b}
+                     onChange={event => this.setState({ b: Number(event.target.value) })}/><br/>
+        </div>
+        <div style={{ display: lambdaVisible }}>
+          <br/>
+          lambda = <input value={this.props.lambda}
+                          onChange={event => this.setState({ lambda: Number(event.target.value) })}/><br/>
+        </div>
+        <div style={{ display: sigmaVisible }}>
+          <br/>
+          sigma = <input value={this.props.sigma}
+                         onChange={event => this.setState({ sigma: Number(event.target.value) })}/><br/>
+        </div>
       </div>
     );
   }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,9 @@ import Path from '@/components/Path';
 import { Distribution } from '@/functions/constants';
 import PathModel from '@/models/PathModel';
 
+const { BINOMIAL_DISTRIBUTION, CONTINIOUS_UNIFORM_DISTRIBUTION, EXPONENTIAL_DISTRIBUTION, GAMMA_DISTRIBUTION, GEOMETRICAL_DISTRIBUTION, NEGATIVE_BINOMIAL_DISTRIBUTION, NORMAL_DISTRIBUTION, PUASSON_DISTRIBUTION } = Distribution;
+
+
 interface IProps {
 }
 
@@ -15,130 +18,66 @@ export default class App extends React.Component<IProps, IState> {
     super(props);
     this.state = {
       pathes: [
-        new PathModel(1, 1, 0, Distribution.NORMAL_DISTRIBUTION, 1, 1, 1, 1, 1, 1, 1, 1),
-        new PathModel(2, 1, 0, Distribution.NORMAL_DISTRIBUTION, 1, 1, 1, 1, 1, 1, 1, 1),
-        new PathModel(3, 1, 0, Distribution.NORMAL_DISTRIBUTION, 1, 1, 1, 1, 1, 1, 1, 1),
-        new PathModel(4, 1, 0, Distribution.NORMAL_DISTRIBUTION, 1, 1, 1, 1, 1, 1, 1, 1),
-        new PathModel(5, 1, 2000, Distribution.NORMAL_DISTRIBUTION, 1, 1, 1, 1, 1, 1, 1, 1),
-        new PathModel(6, 1, 0, Distribution.NORMAL_DISTRIBUTION, 1, 1, 1, 1, 1, 1, 1, 1),
-        new PathModel(7, 1, 0, Distribution.NORMAL_DISTRIBUTION, 1, 1, 1, 1, 1, 1, 1, 1),
-        new PathModel(8, 1, 0, Distribution.NORMAL_DISTRIBUTION, 1, 1, 1, 1, 1, 1, 1, 1),
-        new PathModel(9, 1, 0, Distribution.NORMAL_DISTRIBUTION, 1, 1, 1, 1, 1, 1, 1, 1),
-        new PathModel(10, 1, 0, Distribution.NORMAL_DISTRIBUTION, 1, 1, 1, 1, 1, 1, 1, 1),
-        new PathModel(11, 1, 10000, Distribution.NORMAL_DISTRIBUTION, 1, 1, 1, 1, 1, 1, 1, 1),
-        new PathModel(12, 1, 1000, Distribution.NORMAL_DISTRIBUTION, 1, 1, 1, 1, 1, 1, 1, 1),
-        new PathModel(13, 1, 0, Distribution.NORMAL_DISTRIBUTION, 1, 1, 1, 1, 1, 1, 1, 1),
-        new PathModel(14, 0.05, 0, Distribution.NORMAL_DISTRIBUTION, 1, 1, 1, 1, 1, 1, 1, 1),
-        new PathModel(15, 1, 0, Distribution.NORMAL_DISTRIBUTION, 1, 1, 1, 1, 1, 1, 1, 1),
-        new PathModel(16, 0.95, 0, Distribution.NORMAL_DISTRIBUTION, 1, 1, 1, 1, 1, 1, 1, 1),
-        new PathModel(17, 1, 0, Distribution.NORMAL_DISTRIBUTION, 1, 1, 1, 1, 1, 1, 1, 1),
-        new PathModel(18, 1, 500, Distribution.NORMAL_DISTRIBUTION, 1, 1, 1, 1, 1, 1, 1, 1),
+        new PathModel(1, 1, 0, NORMAL_DISTRIBUTION, 1, 1, 1, 1, 1, 1, 1, 1),
+        new PathModel(2, 1, 0, NORMAL_DISTRIBUTION, 1, 1, 1, 1, 1, 1, 1, 1),
+        new PathModel(3, 1, 0, NORMAL_DISTRIBUTION, 1, 1, 1, 1, 1, 1, 1, 1),
+        new PathModel(4, 1, 0, NORMAL_DISTRIBUTION, 1, 1, 1, 1, 1, 1, 1, 1),
+        new PathModel(5, 1, 2000, NORMAL_DISTRIBUTION, 1, 1, 1, 1, 1, 1, 1, 1),
+        new PathModel(6, 1, 0, NORMAL_DISTRIBUTION, 1, 1, 1, 1, 1, 1, 1, 1),
+        new PathModel(7, 1, 0, NORMAL_DISTRIBUTION, 1, 1, 1, 1, 1, 1, 1, 1),
+        new PathModel(8, 1, 0, NORMAL_DISTRIBUTION, 1, 1, 1, 1, 1, 1, 1, 1),
+        new PathModel(9, 1, 0, NORMAL_DISTRIBUTION, 1, 1, 1, 1, 1, 1, 1, 1),
+        new PathModel(10, 1, 0, NORMAL_DISTRIBUTION, 1, 1, 1, 1, 1, 1, 1, 1),
+        new PathModel(11, 1, 10000, NORMAL_DISTRIBUTION, 1, 1, 1, 1, 1, 1, 1, 1),
+        new PathModel(12, 1, 1000, NORMAL_DISTRIBUTION, 1, 1, 1, 1, 1, 1, 1, 1),
+        new PathModel(13, 1, 0, NORMAL_DISTRIBUTION, 1, 1, 1, 1, 1, 1, 1, 1),
+        new PathModel(14, 0.05, 0, NORMAL_DISTRIBUTION, 1, 1, 1, 1, 1, 1, 1, 1),
+        new PathModel(15, 1, 0, NORMAL_DISTRIBUTION, 1, 1, 1, 1, 1, 1, 1, 1),
+        new PathModel(16, 0.95, 0, NORMAL_DISTRIBUTION, 1, 1, 1, 1, 1, 1, 1, 1),
+        new PathModel(17, 1, 0, NORMAL_DISTRIBUTION, 1, 1, 1, 1, 1, 1, 1, 1),
+        new PathModel(18, 1, 500, NORMAL_DISTRIBUTION, 1, 1, 1, 1, 1, 1, 1, 1),
       ],
     };
   }
 
   render() {
-
-    const { BINOMIAL_DISTRIBUTION, CONTINIOUS_UNIFORM_DISTRIBUTION, EXPONENTIAL_DISTRIBUTION, GAMMA_DISTRIBUTION, GEOMETRICAL_DISTRIBUTION, NEGATIVE_BINOMIAL_DISTRIBUTION, NORMAL_DISTRIBUTION, PUASSON_DISTRIBUTION } = Distribution;
-    const nVisible = (
-      this.state.distribution === BINOMIAL_DISTRIBUTION ||
-      this.state.distribution === NEGATIVE_BINOMIAL_DISTRIBUTION
-    ) ? 'unset' : 'none';
-    const mVisible = this.state.distribution === NORMAL_DISTRIBUTION ? 'unset' : 'none';
-    const rVisible = this.state.distribution === NEGATIVE_BINOMIAL_DISTRIBUTION ? 'unset' : 'none';
-    const aVisible = (
-      this.state.distribution === EXPONENTIAL_DISTRIBUTION ||
-      this.state.distribution === GAMMA_DISTRIBUTION ||
-      this.state.distribution === CONTINIOUS_UNIFORM_DISTRIBUTION
-    ) ? 'unset' : 'none';
-    const bVisible = (
-      this.state.distribution === GAMMA_DISTRIBUTION ||
-      this.state.distribution === CONTINIOUS_UNIFORM_DISTRIBUTION
-    ) ? 'unset' : 'none';
-    const pVisible = (
-      this.state.distribution === BINOMIAL_DISTRIBUTION ||
-      this.state.distribution === GEOMETRICAL_DISTRIBUTION ||
-      this.state.distribution === NEGATIVE_BINOMIAL_DISTRIBUTION
-    ) ? 'unset' : 'none';
-    const lambdaVisible = this.state.distribution === PUASSON_DISTRIBUTION ? 'unset' : 'none';
-    const sigmaVisible = this.state.distribution === NORMAL_DISTRIBUTION ? 'unset' : 'none';
+    const { pathes } = this.state;
     return (
       <main>
         <div style={{ display: 'flex' }}>
-          <div style={{ width: '30%' }}>
-            {/*<img src={"https://github.com/Unerty/gert/blob/main/static/ris4.png?raw=true"} width={'75%'}/>
-        <img src={"https://github.com/Unerty/gert/blob/main/static/ris5.png?raw=true"} width={'25%'}/>*/}
+          <div>
+            {pathes.map(
+              path => <Path id={path.id}
+                            probability={path.probability}
+                            additiveParameter={path.additiveParameter}
+                            distribution={path.distribution}
+                            s={path.s}
+                            m={path.m}
+                            r={path.r}
+                            n={path.n}
+                            a={path.a}
+                            b={path.b}
+                            lambda={path.lambda}
+                            sigma={path.sigma}
+                            setAdditiveParameter={(additiveParameter: number) => {
+                              let pathes = this.state.pathes;
+                              path.additiveParameter = additiveParameter;
+                              this.setState({ pathes: pathes });
+                            }}
+                            setProbability= {(probability: number) => {
+                              let pathes = this.state.pathes;
+                              path.probability = probability;
+                              this.setState({ pathes: pathes });
+                            }}
+                            setDistribution={(distribution: Distribution) => {
+                              let pathes = this.state.pathes;
+                              path.distribution = distribution;
+                              this.setState({ pathes: pathes });
+                            }}
 
-            Оберіть розподіл:<br/>
-            <select value={this.state.distribution}
-                    onChange={event => this.setState({ distribution: event.target.value })}>
-              <option value={BINOMIAL_DISTRIBUTION}>{BINOMIAL_DISTRIBUTION}</option>
-              <option value={EXPONENTIAL_DISTRIBUTION}>{EXPONENTIAL_DISTRIBUTION}</option>
-              <option value={GAMMA_DISTRIBUTION}>{GAMMA_DISTRIBUTION}</option>
-              <option value={GEOMETRICAL_DISTRIBUTION}>{GEOMETRICAL_DISTRIBUTION}</option>
-              <option value={NEGATIVE_BINOMIAL_DISTRIBUTION}>{NEGATIVE_BINOMIAL_DISTRIBUTION}</option>
-              <option value={NORMAL_DISTRIBUTION}>{NORMAL_DISTRIBUTION}</option>
-              <option value={PUASSON_DISTRIBUTION}>{PUASSON_DISTRIBUTION}</option>
-              <option value={CONTINIOUS_UNIFORM_DISTRIBUTION}>{CONTINIOUS_UNIFORM_DISTRIBUTION}</option>
-            </select><br/><br/>
-            Введіть змінні: <br/>
-            {/*s = <input value={this.state.s}
-                     onChange={event => this.setState({ s: Number(event.target.value)})}/><br/>*/}
-            <div style={{ display: nVisible }}>
-              n = <input value={this.state.n}
-                         onChange={event => this.setState({ n: Number(event.target.value) })}/><br/>
-            </div>
-            <div style={{ display: mVisible }}>
-              m = <input value={this.state.m}
-                         onChange={event => this.setState({ m: Number(event.target.value) })}/><br/>
-            </div>
-            <div style={{ display: rVisible }}>
-              r = <input value={this.state.r}
-                         onChange={event => this.setState({ r: Number(event.target.value) })}/><br/>
-            </div>
-            <div style={{ display: aVisible }}>
-              a = <input value={this.state.a}
-                         onChange={event => this.setState({ a: Number(event.target.value) })}/><br/>
-            </div>
-            <div style={{ display: bVisible }}>
-              b = <input value={this.state.b}
-                         onChange={event => this.setState({ b: Number(event.target.value) })}/><br/>
-            </div>
-            <div style={{ display: pVisible }}>
-              p = <input value={this.state.p}
-                         onChange={event => this.setState({ p: Number(event.target.value) })}/><br/>
-            </div>
-            <div style={{ display: lambdaVisible }}>
-              lambda = <input value={this.state.lambda}
-                              onChange={event => this.setState({ lambda: Number(event.target.value) })}/><br/>
-            </div>
-            <div style={{ display: sigmaVisible }}>
-              sigma = <input value={this.state.sigma}
-                             onChange={event => this.setState({ sigma: Number(event.target.value) })}/><br/>
-            </div>
-          </div>
-          <div style={{ width: '70%' }}>
-            <Path id={1} initialProbability={1} initialAdditiveParameter={0}/>
-            <Path id={2} initialProbability={1} initialAdditiveParameter={0}/>
-            <Path id={3} initialProbability={1} initialAdditiveParameter={0}/>
-            <Path id={4} initialProbability={1} initialAdditiveParameter={0}/>
-            <Path id={5} initialProbability={1} initialAdditiveParameter={2000}/>
+              />
+            )}
 
-            <Path id={6} initialProbability={1} initialAdditiveParameter={0}/>
-            <Path id={7} initialProbability={1} initialAdditiveParameter={0}/>
-            <Path id={8} initialProbability={1} initialAdditiveParameter={0}/>
-            <Path id={9} initialProbability={1} initialAdditiveParameter={0}/>
-            <Path id={10} initialProbability={1} initialAdditiveParameter={0}/>
-
-            <Path id={11} initialProbability={1} initialAdditiveParameter={10000}/>
-            <Path id={12} initialProbability={1} initialAdditiveParameter={1000}/>
-            <Path id={13} initialProbability={1} initialAdditiveParameter={0}/>
-            <Path id={14} initialProbability={0.05} initialAdditiveParameter={0}/>
-            <Path id={15} initialProbability={1} initialAdditiveParameter={0}/>
-
-            <Path id={16} initialProbability={0.95} initialAdditiveParameter={0}/>
-            <Path id={17} initialProbability={1} initialAdditiveParameter={0}/>
-            <Path id={18} initialProbability={1} initialAdditiveParameter={500}/>
           </div>
         </div>
         Результати:

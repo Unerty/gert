cec108aabd01fb85ddc20add54597e360d013164
Unerty <johnny.unerty@gmail.com>
2020-12-08 09:26:03 +0200

    Make the Path stateless

--- src/components/Path.tsx
+++ src/components/Path.tsx
@@ -1,45 +1,36 @@
 import * as React from 'react';
+import PathModel from '@/models/PathModel';
 
-interface IProps {
-  id: number;
-  initialProbability: number;
-  initialAdditiveParameter: number;
+interface IProps extends PathModel {
+  setAdditiveParameter: (additiveParameter: number) => void;
+  setProbability: (probability: number) => void;
+  setDistribution: (distribution: string) => void;
 }
 
-interface IState {
-  probability: number;
-  additiveParameter: number;
-}
-
-export default class Path extends React.Component<IProps, IState> {
+export default class Path extends React.Component<IProps> {
   constructor(props) {
     super(props);
-    this.state = {
-      probability: props.initialProbability,
-      additiveParameter: props.initialAdditiveParameter
-    };
   }
 
   render() {
-    const {id} = this.props;
-    const {probability, additiveParameter} = this.state;
+    const { id, probability, additiveParameter, distribution, setAdditiveParameter, setProbability, setDistribution } = this.props;
     return (
       <div style={{
         backgroundColor: 'sandybrown',
         display: 'inline-block',
         width: '150px',
         margin: '0.5rem',
-        padding: '0.25rem'
+        padding: '0.25rem',
       }}>
         <h3>Номер дуги: {id}</h3>
         <span>Вартість, грн.:<br/></span>
-        <input type="number" onChange={event => this.setState({ additiveParameter: Number(event.target.value) })}
+        <input type="number" onChange={event => setAdditiveParameter(Number(event.target.value))}
                value={additiveParameter.toString()} min={0} step={0.01}
-               style={{width: '100px'}}
+               style={{ width: '100px' }}
         />
         <br/>
         <span>Ймовірність: <strong> {probability}</strong><br/></span>
-        <input type={'range'} onChange={event => this.setState({ probability: Number(event.target.value) })}
+        <input type={'range'} onChange={event => setProbability(Number(event.target.value))}
                value={probability.toString()} min={0} max={1} step={0.01}/>
       </div>
     );

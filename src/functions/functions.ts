7f51d686bf6cc7f58de8b49b4d9bfb340ac7d530
Unerty <johnny.unerty@gmail.com>
2020-12-10 09:31:24 +0200

    Fill wFunction

--- src/functions/functions.ts
+++ src/functions/functions.ts
@@ -1,5 +1,5 @@
-export function wFunction() {
-  return ;
+export function wFunction(W1, W2, W3, W4, W5, W6, W7, W8, W9, W10, W11, W12, W13, W14, W15, W16, W17, W18) {
+  return ((W1*W3 + W2*W4*W5*W6)*W7*W8(W9*W11 + W10*W12) * W13*W16*W17*W18) / ((1 - W8*(W9*W11 + W10*W12)*W13*W14*W15));
 }
 
 export function binomialDistributionMomentGeneratingFunction(s: number, n: number, p: number) {

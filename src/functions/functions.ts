5cbaab0fbfbdb2d62c5866c15ec12542dadfb61f
Unerty <johnny.unerty@gmail.com>
2020-12-16 21:02:24 +0200

    Rename wEsFunction

--- src/functions/functions.ts
+++ src/functions/functions.ts
@@ -1,7 +1,7 @@
 import { Distribution } from '@/functions/constants';
 import PathModel from '@/models/PathModel';
 
-export function masonFunction(W1, W2, W3, W4, W5, W6, W7, W8, W9, W10, W11, W12, W13, W14, W15, W16, W17, W18) {
+export function wEsFunction(W1, W2, W3, W4, W5, W6, W7, W8, W9, W10, W11, W12, W13, W14, W15, W16, W17, W18) {
   return ((W1 * W3 + W2 * W4 * W5 * W6) * W7 * W8(W9 * W11 + W10 * W12) * W13 * W16 * W17 * W18) / ((1 - W8 * (W9 * W11 + W10 * W12) * W13 * W14 * W15));
 }
 

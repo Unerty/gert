80fb7cee4777b4c1bd8ed32e84038a3809df5862
Unerty <johnny.unerty@gmail.com>
2020-12-17 17:09:47 +0200

    Add second moment and expected value functions

--- src/functions/functions.ts
+++ src/functions/functions.ts
@@ -2,7 +2,7 @@ import { Distribution } from '@/functions/constants';
 import PathModel from '@/models/PathModel';
 
 export function wEsFunction(W1, W2, W3, W4, W5, W6, W7, W8, W9, W10, W11, W12, W13, W14, W15, W16, W17, W18) {
-  return ((W1 * W3 + W2 * W4 * W5 * W6) * W7 * W8*(W9 * W11 + W10 * W12) * W13 * W16 * W17 * W18) / ((1 - W8 * (W9 * W11 + W10 * W12) * W13 * W14 * W15));
+  return ((W1 * W3 + W2 * W4 * W5 * W6) * W7 * W8 * (W9 * W11 + W10 * W12) * W13 * W16 * W17 * W18) / ((1 - W8 * (W9 * W11 + W10 * W12) * W13 * W14 * W15));
 }
 
 export function binomialDistributionMomentGeneratingFunction(s: number, n: number, p: number) {
@@ -41,11 +41,78 @@ export function continuousUniformDistributionMomentGeneratingFunction(s: number,
   return (nominator / denominator);
 }
 
+
+export function binomialDistributionExpectedValueFunction(n: number, p: number) {
+  return n * p;
+}
+
+export function exponentialDistributionExpectedValueFunction(a: number) {
+  return 1 / a;
+}
+
+export function gammaDistributionExpectedValueFunction(b: number, a: number) {
+  return b / a;
+}
+
+export function geometricalDistributionExpectedValueFunction(p: number) {
+  return 1 / p;
+}
+
+export function negativeBinomialDistributionExpectedValueFunction(r: number, p: number) {
+  return (r * (1 - p)) / p;
+}
+
+export function normalBinomialDistributionExpectedValueFunction(m: number) {
+  return m;
+}
+
+export function puassonBinomialDistributionExpectedValueFunction(lambda: number) {
+  return lambda;
+}
+
+export function continuousUniformDistributionExpectedValueFunction(a: number, b: number) {
+  return (a + b) / 2;
+}
+
+
+export function binomialDistributionSecondMomentFunction(n: number, p: number) {
+  return n * p * (n * p + 1 - p);
+}
+
+export function exponentialDistributionSecondMomentFunction(a: number) {
+  return 2 / (a * a);
+}
+
+export function gammaDistributionSecondMomentFunction(b: number, a: number) {
+  return b * (b + 1) / (a * a);
+}
+
+export function geometricalDistributionSecondMomentFunction(p: number) {
+  return (2 - p) / (p * p);
+}
+
+export function negativeBinomialDistributionSecondMomentFunction(r: number, p: number) {
+  return (r * (1 - p) * (1 + r - r * p)) / (p * p);
+}
+
+export function normalBinomialDistributionSecondMomentFunction(m: number, sigma: number) {
+  return m * m + sigma * sigma;
+}
+
+export function puassonBinomialDistributionSecondMomentFunction(lambda: number) {
+  return lambda * (1 + lambda);
+}
+
+export function continuousUniformDistributionSecondMomentFunction(a: number, b: number) {
+  return (a * a + a * b + b * b) / 3;
+}
+
+
 export function calculateWFunctionForPath(path: PathModel, isSEqualZero: boolean) {
   let moment: number;
   let W: number;
   const { a, distribution, sigma, lambda, probability, additiveParameter, b, m, n, r } = path;
-  let s = isSEqualZero? 0 : path.s;
+  let s = isSEqualZero ? 0 : path.s;
   const { BINOMIAL_DISTRIBUTION, CONTINUOUS_UNIFORM_DISTRIBUTION, EXPONENTIAL_DISTRIBUTION, GAMMA_DISTRIBUTION, GEOMETRICAL_DISTRIBUTION, NEGATIVE_BINOMIAL_DISTRIBUTION, NORMAL_DISTRIBUTION, PUASSON_DISTRIBUTION } = Distribution;
   switch (distribution) {
     case BINOMIAL_DISTRIBUTION: {
